aa0300bac7e3110e53ba49198ebdfc6f43bcc591
rannyara <rannyara_12@hotmail.com>
2025-08-27 03:10:28 -0300

    User info update scenario added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -7,17 +7,41 @@ describe('Orange HRM Test', () => {
     passwordField: '[name="password"]',
     loginButton: '.oxd-button',
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    wrongCredentialAlert: '.oxd-alert'
+    wrongCredentialAlert: '.oxd-alert',
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: '[name="firstName"]',
+    middleNameField: '[name="middleName"]',
+    lastNameField: '[name="lastName"]',
+    genericField: '.oxd-input',
+    dateField: '.oxd-date-input',
+    dateCloseButton: '.--close',
+    submitButton: '.oxd-button',
+    successfullyMessage: '.oxd-toast'
+
   }
 
 
-  it('Login - sucess', () => {
+  it('User Info Update - Success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')  
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorsList.middleNameField).clear().type('middleNameTest')
+    cy.get(selectorsList.lastNameField).clear().type('lastNameTest')
+    cy.get(selectorsList.genericField).eq(4).clear().type('"employee')
+    cy.get(selectorsList.genericField).eq(5).clear().type('otherIdTest')   
+    cy.get(selectorsList.genericField).eq(6).clear().type('driversLicenceNumberTest')
+    cy.get(selectorsList.dateField).eq(0).clear().type('2027-12-12')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.dateField).eq(1).clear().type('1992-07-12')
+    cy.get(selectorsList.dateCloseButton).click() 
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get(selectorsList.successfullyMessage).should('contain', 'Success')
+    cy.get('.oxd-toast-close')     
   })
 
   it('Login - fail', () => {
